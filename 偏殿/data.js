258b5676fbbf1c3fc747e1ff0963dc4acbf908d6
Hsiung11433 <windh4xg@gmail.com>
2026-04-30 10:35:37 +0800

    0430

---
+++ 偏殿/data.js
@@ -0,0 +1,395 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "前殿入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.8188148296396527,
+        "pitch": -0.040960152254323035,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.487400445028488,
+          "pitch": -0.12260917518388048,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8638944463907716,
+          "pitch": -0.25437156746173173,
+          "title": "前殿入口",
+          "text": "前殿原是作為神廟入口，被保存至今，其中建築結構大致完整"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "前殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "yaw": 1.873121695080413,
+        "pitch": 0.040374382606570336,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6138438254384804,
+          "pitch": 0.5868428027473573,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.74450410187187,
+          "pitch": 0.018017362764013,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "前殿出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "yaw": 1.7889623777812682,
+        "pitch": 0.1585860096883085,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3754577366507483,
+          "pitch": 0.5656067035560888,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.7298387023587436,
+          "pitch": 0.12553415257915823,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9683317029212875,
+          "pitch": -0.24044516948209171,
+          "title": "正殿",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.196403033545801,
+          "pitch": -0.18234661891499115,
+          "title": "偏殿",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "中央空地",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "yaw": 1.8553836784394786,
+        "pitch": 0.08389540280484731,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3968483000218903,
+          "pitch": 0.5515425320174767,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.3885818572316264,
+          "pitch": -0.019427572205406918,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "偏殿入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.0923020701545667,
+        "pitch": 0.20580593978725936,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9562638450311347,
+          "pitch": 0.5192906616789692,
+          "rotation": 7.0685834705770345,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.20369353742896124,
+          "pitch": 0.049384675044219506,
+          "rotation": 0.7853981633974483,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2727591744185887,
+          "pitch": -0.10316165525661347,
+          "title": "人形石柱",
+          "text": "古希臘傳統藝術，將支撐牆體的柱子雕塑成人型雕像"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "偏殿後方",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 0.78342454113392,
+        "pitch": -0.04018513053709327,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8869384357825627,
+          "pitch": 0.6253719997869602,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.12322221342000361,
+          "pitch": -0.07789149929811856,
+          "rotation": 0.7853981633974483,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3705394898079089,
+          "pitch": -0.3793305655247501,
+          "title": "人形石柱",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "偏殿正面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 972,
+      "initialViewParameters": {
+        "yaw": -2.9113665999636567,
+        "pitch": -0.43416031865723426,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6560266603966873,
+          "pitch": 0.11859065906191191,
+          "rotation": 5.497787143782138,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.997084858271962,
+          "pitch": 0.020879341787431116,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "偏殿側面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 972,
+      "initialViewParameters": {
+        "yaw": 0.9761307487230759,
+        "pitch": 0.042746957653397644,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5006572777969422,
+          "pitch": 0.36074651304302563,
+          "rotation": 0.7853981633974483,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.01577916749814534,
+          "pitch": 0.098894166256704,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "偏殿側面連接中央空地",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.45592274337810323,
+        "pitch": -0.1626963458280386,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0821435321245971,
+          "pitch": 0.1259399057753594,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.5658621056129611,
+          "pitch": 0.00735416974659131,
+          "rotation": 7.0685834705770345,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "前殿加偏殿",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
---
+++ 正殿/data.js
@@ -0,0 +1,444 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "正殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 720,
+      "initialViewParameters": {
+        "yaw": -2.182105897429878,
+        "pitch": -0.2193481143468734,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.043881690600582,
+          "pitch": 0.11416188782903802,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.196761358904757,
+          "pitch": -0.41891342547518917,
+          "title": "神廟正殿",
+          "text": "景點區內最大的神廟建築"
+        },
+        {
+          "yaw": 2.3617177103927656,
+          "pitch": -0.038314232375727286,
+          "title": "偏殿",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "正殿側後方",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 2.060705039721813,
+        "pitch": -0.07395288446730497,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.919738945293785,
+          "pitch": 0.3017930519739984,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.22918697208279326,
+          "pitch": 0.3771356973637374,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6594566500672343,
+          "pitch": -0.31390446658807036,
+          "title": "牆體浮雕",
+          "text": "神廟牆體原有大量浮雕裝飾，大部分已受損，僅剩殘餘浮雕仍留於博物館內或牆體上"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "正殿後方",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.9561980442376932,
+        "pitch": -0.19975376710902992,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3003621624132045,
+          "pitch": 0.49379488666346205,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.73086448064298,
+          "pitch": 0.25987265244615365,
+          "rotation": 7.0685834705770345,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7035359265030703,
+          "pitch": -0.7157576244080772,
+          "title": "牆體浮雕",
+          "text": "神廟牆體原有大量浮雕裝飾，大部分已受損，僅剩殘餘浮雕仍留於博物館內或牆體上"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "正殿正後方",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.7314054750303658,
+        "pitch": -0.015473218903933272,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8420616824646254,
+          "pitch": 0.35745036185084444,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.08804839191084213,
+          "pitch": 0.2594519348862647,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "正殿左側後",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.1804722032938395,
+        "pitch": 0.28559809607742537,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9516434887566927,
+          "pitch": 0.33612240634530366,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.6906309038142826,
+          "pitch": 0.19629274514746697,
+          "rotation": 0.7853981633974483,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "正殿左側布告欄展區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.1317995224435187,
+        "pitch": -0.0053677070994258,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1506830979464162,
+          "pitch": 0.03153233250082721,
+          "rotation": 5.497787143782138,
+          "target": "6-"
+        },
+        {
+          "yaw": 3.043811620096748,
+          "pitch": 0.38265589073929185,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0337048372442297,
+          "pitch": 0.13421771626352452,
+          "title": "文字內容展區",
+          "text": "講述簡單古希臘歷史背景"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "正殿左前方",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.8455570064589537,
+        "pitch": 0.0442895232221403,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.928480131443264,
+          "pitch": 0.48374736829217824,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.25903598246298465,
+          "pitch": 0.016072791736259973,
+          "rotation": 7.0685834705770345,
+          "target": "7-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-1",
+      "name": "正殿前方1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 2.430508602127909,
+        "pitch": 0.18498059991728688,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5854998962592948,
+          "pitch": 0.32980803984738927,
+          "rotation": 5.497787143782138,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.12974425989937366,
+          "pitch": 0.05672598161417497,
+          "rotation": 0,
+          "target": "8-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4232175268036187,
+          "pitch": -0.07969204667409002,
+          "title": "支撐鷹架",
+          "text": "由於年代久遠導致結構不穩，因此需長期由鷹架支撐"
+        }
+      ]
+    },
+    {
+      "id": "8-1",
+      "name": "出口往前殿1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.014214826295742,
+          "pitch": 0.37327466012054344,
+          "rotation": 0.7853981633974483,
+          "target": "7-1"
+        },
+        {
+          "yaw": 0.047715048198408994,
+          "pitch": 0.10101421767993202,
+          "rotation": 0.7853981633974483,
+          "target": "9-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-2",
+      "name": "出口往前殿2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7800642581300963,
+          "pitch": 0.34904688779210247,
+          "rotation": 0,
+          "target": "8-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "正殿",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
