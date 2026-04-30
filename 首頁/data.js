b9cdb6eb18b40fde544a3213b27bc02cb5d4da6b
Hsiung11433 <windh4xg@gmail.com>
2026-04-30 11:11:43 +0800

    0430

---
+++ 首頁/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "全知視角-首頁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.12408958678710036,
+          "pitch": 0.35147249555433646,
+          "title": "雅典衛城",
+          "text": "雅典衛城現存的建築群共包含幾座具有重大建築和歷史意義的古代建築遺跡，其中最著名的是帕德嫩神廟"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
